e9bd741b82af19bd148293120a40151b5a88cc4c
Hecker_01 <jesseflantua@icloud.com>
2023-10-28 20:19:48 +0200

    Renamed from Color.java to TextColor.java

---
+++ src/main/java/net/heckerdev/secretlife/util/TextColor.java
@@ -0,0 +1,21 @@
+package net.heckerdev.secretlife.util;
+
+
+public class TextColor {
+    public static final net.kyori.adventure.text.format.TextColor BLACK = net.kyori.adventure.text.format.TextColor.color(0x000000);
+    public static final net.kyori.adventure.text.format.TextColor DARK_BLUE = net.kyori.adventure.text.format.TextColor.color(0x0000AA);
+    public static final net.kyori.adventure.text.format.TextColor DARK_GREEN = net.kyori.adventure.text.format.TextColor.color(0x00AA00);
+    public static final net.kyori.adventure.text.format.TextColor DARK_AQUA = net.kyori.adventure.text.format.TextColor.color(0x00AAAA);
+    public static final net.kyori.adventure.text.format.TextColor DARK_RED = net.kyori.adventure.text.format.TextColor.color(0xAA0000);
+    public static final net.kyori.adventure.text.format.TextColor DARK_PURPLE = net.kyori.adventure.text.format.TextColor.color(0xAA00AA);
+    public static final net.kyori.adventure.text.format.TextColor GOLD = net.kyori.adventure.text.format.TextColor.color(0xFFAA00);
+    public static final net.kyori.adventure.text.format.TextColor GRAY = net.kyori.adventure.text.format.TextColor.color(0xAAAAAA);
+    public static final net.kyori.adventure.text.format.TextColor DARK_GRAY = net.kyori.adventure.text.format.TextColor.color(0x555555);
+    public static final net.kyori.adventure.text.format.TextColor BLUE = net.kyori.adventure.text.format.TextColor.color(0x5555FF);
+    public static final net.kyori.adventure.text.format.TextColor GREEN = net.kyori.adventure.text.format.TextColor.color(0x55FF55);
+    public static final net.kyori.adventure.text.format.TextColor AQUA = net.kyori.adventure.text.format.TextColor.color(0x55FFFF);
+    public static final net.kyori.adventure.text.format.TextColor RED = net.kyori.adventure.text.format.TextColor.color(0xFF5555);
+    public static final net.kyori.adventure.text.format.TextColor MAGENTA = net.kyori.adventure.text.format.TextColor.color(0xFF55FF);
+    public static final net.kyori.adventure.text.format.TextColor YELLOW = net.kyori.adventure.text.format.TextColor.color(0xFFFF55);
+    public static final net.kyori.adventure.text.format.TextColor WHITE = net.kyori.adventure.text.format.TextColor.color(0xFFFFFF);
+}
